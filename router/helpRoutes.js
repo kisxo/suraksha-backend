fe15f209ec397f23f3d47a21c30bb51d0d70013f
uday <udaysubba2004@gmail.com>
2025-03-20 11:40:39 +0530

    update routes

--- router/helpRoutes.js
+++ router/helpRoutes.js
@@ -1,22 +1,25 @@
 import express from "express";
+import { createHelp } from "../controllers/helpController.js";
 
 const helpRouter = express.Router();
 
-helpRouter.get("/", async (req, res) => {
-      console.log(req.body)
-      return res.status(200).send("gg")
-});
+helpRouter.post('/', createHelp)
 
-helpRouter.post("/", async (req, res) => {
-      console.log(req.body)
-      return res.status(200).send("gg")
-});
+// helpRouter.get("/", async (req, res) => {
+//       console.log(req.body)
+//       return res.status(200).send("gg")
+// });
 
-helpRouter.post("/locations", async (req, res) => {
-      console.log('locations')
-      console.log(req.body)
-      return res.status(200).send("gg")
-});
+// helpRouter.post("/", async (req, res) => {
+//       console.log(req.body)
+//       return res.status(200).send("gg")
+// });
+
+// helpRouter.post("/locations", async (req, res) => {
+//       console.log('locations')
+//       console.log(req.body)
+//       return res.status(200).send("gg")
+// });
 
 
 export default helpRouter;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ import userRouter from "./router/userRoutes.js";
 app.use("/api/users/", userRouter);
 
 import helpRouter from "./router/helpRoutes.js"
-app.use("/api/help/", helpRouter);
+app.use("/api/helps/", helpRouter);
 
 
 app.listen(3000, () => {
