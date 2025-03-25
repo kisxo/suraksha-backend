c3cccbe432e8d597bd6987c19834c890d9f9fc0c
uday <udaysubba2004@gmail.com>
2025-03-25 12:05:38 +0530

    add socket io support

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import connectDB from "./config/database.js";
 import { configDotenv } from "dotenv";
+import cors from "cors";
 
 // dotenv
 configDotenv();
 connectDB();
 const app = express()
+const httpServer = createServer( app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "*",
+	}
+});
 app.use(express.json())
+app.use(cors());
 
 import userRouter from "./router/userRoutes.js";
 app.use("/api/users/", userRouter);
@@ -15,6 +25,12 @@ import helpRouter from "./router/helpRoutes.js"
 app.use("/api/helps/", helpRouter);
 
 
-app.listen(3000, () => {
-	console.log("listen");
+io.on('connection', (socket) => {
+	console.log('user connected');  socket.on('disconnect', function () {
+	  console.log('user disconnected');
+	});
 })
+  
+httpServer.listen(3000, () => {
+	console.log(`Listening on port 3000`);
+}); 
